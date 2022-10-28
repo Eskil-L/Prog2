e02d896d65317f46b46623131fc334ec9026bb04
etienne20044 <92087153+etienne20044@users.noreply.github.com>
2022-10-28 10:37:16 +0200

    Senaste

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,163 +6,183 @@ using System.Security.Cryptography;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
         static List<List<string>> boardArrayList = new List<List<string>>();
 
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string board = "[T, H, B, K, Q, B, H, T]\n[P, P, P, P, P, P, P, P]\n[', ', ', ', ', ', ', ']\n[', ', ', ', ', ', ', ']\n[', ', ', ', ', ', ', ']\n[', ', ', ', ', ', ', ']\n[p, p, p, p, p, p, p, p]\n[t, h, b, k, q, b, h, t]";
+
+
+            var row1 = new List<string>() { "T", "H", "B", "K", "Q", "B", "H", "T" };
+            var row2 = new List<string>() { "P", "P", "P", "P", "P", "P", "P", "P" };
+            var row3 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
+            var row4 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
+            var row5 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
+            var row6 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
+            var row7 = new List<string>() { "p", "p", "p", "p", "p", "p", "p", "p" };
+            var row8 = new List<string>() { "t", "h", "b", "k", "q", "b", "h", "t" };
+
+          
+
+
+            
+
+
+
+            boardArrayList.Add(row1);
+            boardArrayList.Add(row2);
+            boardArrayList.Add(row3);
+            boardArrayList.Add(row4);
+            boardArrayList.Add(row5);
+            boardArrayList.Add(row6);
+            boardArrayList.Add(row7);
+            boardArrayList.Add(row8);
+
+
+            /*var roww2 = boardArrayList[1];
+            rad[0] = "'";
+
+            var roww3 = boardArrayList[2];
+            roww3[0] = "P";*/
+
+
+
+            //Console.WriteLine(boardArrayList[0][1]);
+
+            Console.WriteLine("╻━━━━━━━━━BLACK━━━━━━━━━━╻");
+
+            foreach (var row in boardArrayList)
+            {
+                Console.Write("| ");
+                foreach (var item in row)
+                {
+                    Console.Write(item);
+                    Console.Write(" |");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("╹━━━━━━━━━WHITE━━━━━━━━━━╹");
+
+
+
+
+
+            //DrawBoard();
+
+            Console.WriteLine("\nWelcome to the chess game!\n");
+            Console.WriteLine("\nDo you know the gamerules?\n");
+            string input = Console.ReadLine().ToLower();
+
+            // exempel input:
+            /*
+
+            a1 a5 -> flytta pjäsen på a1 till a5
+            - om det går att flytta så gör vi det
+            - om det inte går ?
+                - 
+            - om det inte finns en pjäs på a1 
+                - 
+
+             */
+
+            if (input == "yes")
+            {
+                Console.WriteLine("\nThe game begins.");
+            }
+
+            else if (input == "no")
+            {
+                Console.WriteLine("\nGoogle them then.");
+            }
+
+            else
+            {
+                Console.WriteLine("Write yes/no");
+            }
+
+
+            Console.WriteLine("Enter the piece you want to move:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter the position you want to move the piece to:");
+            string input2 = Console.ReadLine();
+
+            char letter = input1[0];
+
+            Console.WriteLine("input[1]: " + input1[1]);
+            int number = Int32.Parse(input1[1] + "");
+            Console.WriteLine("number: " + number);
+            int letterToIndex = ConvertLetterToIndex(letter);
+
+
+            int index = 8 - number;
+
+            Console.WriteLine("index: " + index);
+
+            var rad = boardArrayList[index];
+            var position = rad[letterToIndex];
+
+            rad[letterToIndex] = "'";
+
+
+            Console.WriteLine("╻━━━━━━━━━BLACK━━━━━━━━━━╻");
+
+            foreach (var row in boardArrayList)
+            {
+                Console.Write("| ");
+                foreach (var item in row)
+                {
+                    Console.Write(item);
+                    Console.Write(" |");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("╹━━━━━━━━━WHITE━━━━━━━━━━╹");
 
         }
-        
 
-        static int ConvertLetterToIndex(string letter)
+
+        static int ConvertLetterToIndex(char letter)
         {
             // test
-            if (letter == "a")
+            if (letter == 'a')
             {
                 return 0;
             }
-            else if (letter == "b")
+            else if (letter == 'b')
             {
                 return 1;
             }
-            else if (letter == "c")
+            else if (letter == 'c')
             {
                 return 2;
             }
-            else if (letter == "d")
+            else if (letter == 'd')
             {
                 return 3;
             }
-            else if (letter == "e")
+            else if (letter == 'e')
             {
                 return 4;
             }
-            else if (letter == "f")
+            else if (letter == 'f')
             {
                 return 5;
             }
-            else if (letter == "g")
+            else if (letter == 'g')
             {
                 return 6;
             }
-            else if (letter == "h")
+            else if (letter == 'h')
             {
                 return 7;
             }
             else
             {
                 return -1;
-            }
-
-
-            static void Main(string[] args)
-            {
-                Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-                string board = "[T, H, B, K, Q, B, H, T]\n[P, P, P, P, P, P, P, P]\n[', ', ', ', ', ', ', ']\n[', ', ', ', ', ', ', ']\n[', ', ', ', ', ', ', ']\n[', ', ', ', ', ', ', ']\n[p, p, p, p, p, p, p, p]\n[t, h, b, k, q, b, h, t]";
-
-
-                var row1 = new List<string>() { "T", "H", "B", "K", "Q", "B", "H", "T" };
-                var row2 = new List<string>() { "P", "P", "P", "P", "P", "P", "P", "P" };
-                var row3 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
-                var row4 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
-                var row5 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
-                var row6 = new List<string>() { "'", "'", "'", "'", "'", "'", "'", "'" };
-                var row7 = new List<string>() { "p", "p", "p", "p", "p", "p", "p", "p" };
-                var row8 = new List<string>() { "t", "h", "b", "k", "q", "b", "h", "t" };
-               
-                
-                string input1 = Console.ReadLine("Position move from");
-
-                string input2 = Console.Readline("Position move to");
-
-
-                string input1[0] = letter;
-                int input1[1] = number;
-
-
-                newNumber = 8 - number;
-
-                rad = boardArrayList[index];
-        
-
-                    
-
-
-                boardArrayList.Add(row1);
-                boardArrayList.Add(row2);
-                boardArrayList.Add(row3);
-                boardArrayList.Add(row4);
-                boardArrayList.Add(row5);
-                boardArrayList.Add(row6);
-                boardArrayList.Add(row7);
-                boardArrayList.Add(row8);
-
-                var roww2 = boardArrayList[1];
-                roww2[0] = "'";
-
-                var roww3 = boardArrayList[2];
-                roww3[0] = "P";
-
-
-
-                //Console.WriteLine(boardArrayList[0][1]);
-
-                Console.WriteLine("╻━━━━━━━━━BLACK━━━━━━━━━━╻");
-
-                foreach (var row in boardArrayList)
-                {
-                    Console.Write("| ");
-                    foreach (var item in row)
-                    {
-                        Console.Write(item);
-                        Console.Write(" |");
-                    }
-                    Console.Write("\n");
-                }
-                Console.WriteLine("╹━━━━━━━━━WHITE━━━━━━━━━━╹");
-
-
-
-
-
-                //DrawBoard();
-
-                Console.WriteLine("\nWelcome to the chess game!\n");
-                Console.WriteLine("\nDo you know the gamerules?\n");
-                string input = Console.ReadLine().ToLower();
-
-                // exempel input:
-                /*
-
-                a1 a5 -> flytta pjäsen på a1 till a5
-                - om det går att flytta så gör vi det
-                - om det inte går ?
-                    - 
-                - om det inte finns en pjäs på a1 
-                    - 
-
-                 */
-
-                if (input == "yes")
-                {
-                    Console.WriteLine("\nThe game begins.");
-                }
-
-                else if (input == "no")
-                {
-                    Console.WriteLine("\nGoogle them then.");
-                }
-
-                else
-                {
-                    Console.WriteLine("Write yes/no");
-                }
-
-
-
             }
         }
     }
